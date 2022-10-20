71d4bc0a3a557d4ff678d1c8dc5f1e8a0e0ea4f8
Suryadev25 <suryads5052@gmail.com>
2022-10-21 01:29:35 +0530

    Tree

--- graphs_trees.cpp
+++ graphs_trees.cpp
@@ -254,3 +254,398 @@
 // }
 
 //**************** Cycle Detection *********************
+// #include <bits/stdc++.h>
+// #define ll long long
+// #define int int64_t
+// using namespace std;
+
+// bool DFS(int vertex, int parent, vector<int> g[], bool vis[])
+// {
+//     vis[vertex] = true;
+//     bool check_loop = false;
+//     for (auto x : g[vertex])
+//     {
+//         if (vis[x] == 1 && x == parent)
+//             continue;
+//         if (vis[x] == 1 && x != parent)
+//             return true;
+//         check_loop |= DFS(x, vertex, g, vis);
+//     }
+//     return check_loop;
+// }
+// bool is_cycle(int vertex, vector<int> g[])
+// {
+//     bool vis[vertex + 10];
+//     bool check_loop = false;
+//     for (int i = 0; i < vertex; i++)
+//     {
+//         vis[i] = 0;
+//     }
+//     for (int i = 0; i < vertex; i++)
+//     {
+//         if (vis[i])
+//             continue;
+//         if (DFS(i, 0, g, vis))
+//         {
+//             check_loop = true;
+//             break;
+//         }
+//     }
+//     return check_loop;
+// }
+// int32_t main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     int n, m;
+//     cin >> n >> m;
+//     vector<int> g[n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         int v1, v2;
+//         cin >> v1 >> v2;
+//         g[v1].push_back(v2);
+//         g[v2].push_back(v1);
+//     }
+//     bool ans = is_cycle(n, g);
+//     cout << ans << endl;
+//     return 0;
+// }
+
+// ******************Adjancency Matrix*******************************
+// #include <bits/stdc++.h>
+// #define ll long long
+// #define int int64_t
+// using namespace std;
+// void DFS(int i, int j, int initial, int newcolor, vector<vector<int>> &image)
+// {
+//     int n = image.size();
+//     int m = image[0].size();
+//     if (i < 0 || j < 0)
+//         return;
+//     if (i >= n || j >= m)
+//         return;
+//     if (image[i][j] != initial)
+//         return;
+
+//     image[i][j] = newcolor;
+
+//     DFS(i + 1, j, initial, newcolor, image);
+//     DFS(i - 1, j, initial, newcolor, image);
+//     DFS(i, j + 1, initial, newcolor, image);
+//     DFS(i, j - 1, initial, newcolor, image);
+// }
+// vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color)
+// {
+
+//     int initial = image[sr][sc];
+//     if (initial != color)
+//         DFS(sr, sc, initial, color, image);
+//     return image;
+// }
+// int32_t main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     // Take Input
+//     return 0;
+// }
+
+// *********************NO of Islands**********************
+// class Solution
+// {
+// public:
+//     void DFS(int i, int j, vector<vector<char>> &grid)
+//     {
+
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         if (i < 0 || j < 0)
+//             return;
+//         if (i >= n || j >= m)
+//             return;
+//         if (grid[i][j] != '1')
+//             return;
+
+//         grid[i][j] = '0';
+
+//         DFS(i + 1, j, grid);
+//         DFS(i - 1, j, grid);
+//         DFS(i, j + 1, grid);
+//         DFS(i, j - 1, grid);
+//         DFS(i+1,j+1,grid);
+//         DFS(i-1,j-1,grid);
+//         DFS(i-1,j+1,grid);
+//         DFS(i+1,j-1,grid);
+//     }
+//     int numIslands(vector<vector<char>> &grid)
+//     {
+//         int count = 0;
+//         for (int i = 0; i < grid.size(); i++)
+//         {
+//             for (int j = 0; j < grid[0].size(); j++)
+//             {
+//                 if (grid[i][j] == '1')
+//                 {
+
+//                     DFS(i, j, grid);
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+// ***************DFS in A tree (Depth / Height)***************
+
+// #include <bits/stdc++.h>
+// #define ll long long
+// #define int int64_t
+// using namespace std;
+
+// const int N = 1e5 + 10;
+// vector<int> g[N];
+// int depth[N], height[N];
+// void DFS(int vertex, int parent = 0)
+// {
+//     for (auto x : g[vertex])
+//     {
+//         if (x == parent)
+//             continue;
+//         depth[x] = depth[vertex] + 1;
+//         DFS(x, vertex);
+//         height[vertex] = max(height[vertex], height[x] + 1);
+//     }
+// }
+// int32_t main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int x, y;
+//         cin >> x >> y;
+//         g[x].push_back(y);
+//         g[y].push_back(x);
+//     }
+//     DFS(1);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         cout << depth[i] << " " << height[i] << endl;
+//     }
+//     return 0;
+// }
+// *****************Sum of Subtree & Even Count*********************
+// #include <bits/stdc++.h>
+// #define ll long long
+// #define int int64_t
+// using namespace std;
+
+// const int N = 1e5 + 10;
+// vector<int> g[N];
+// int sum_subtree[N];
+// int even_count[N];
+
+// void DFS(int vertex, int parent = 0)
+// {
+//     sum_subtree[vertex] += vertex;
+//     if (vertex % 2 == 0)
+//     {
+//         even_count[vertex]++;
+//     }
+//     for (auto x : g[vertex])
+//     {
+//         if (x == parent)
+//             continue;
+//         DFS(x, vertex);
+//         even_count[vertex] += even_count[x];
+//         sum_subtree[vertex] += sum_subtree[x];
+//     }
+// }
+// int32_t main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int x, y;
+//         cin >> x >> y;
+//         g[x].push_back(y);
+//         g[y].push_back(x);
+//     }
+//     DFS(1);
+//     for (int i = 1; i <= n; i++)
+//     {
+//         cout << sum_subtree[i] << " " << even_count[i] << endl;
+//     }
+//     return 0;
+// }
+// *********************Diameter of Tree**********************
+// #include <bits/stdc++.h>
+// #define ll long long
+// #define int int64_t
+// using namespace std;
+// const int N = 1e5 + 10;
+// vector<int> g[N];
+// int depth[N];
+
+// void DFS(int vertex, int parent = 0)
+// {
+//     for (auto x : g[vertex])
+//     {
+//         if (x == parent)
+//             continue;
+//         depth[x] = depth[vertex] + 1;
+//         DFS(x, vertex);
+//     }
+// }
+
+// int32_t main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int x, y;
+//         cin >> x >> y;
+//         g[x].push_back(y);
+//         g[y].push_back(x);
+//     }
+//     int max_dep = -1;
+//     int deep = 1;
+// for (int i = 1; i <= n; i++)
+// {
+//     if (max_dep < depth[i])
+//     {
+//         max_dep = depth[i];
+//         deep = i;
+//     }
+// depth[i] = 0;
+// }
+// DFS(deep);
+// max_dep = -1;
+// for (int i = 1; i <= n; i++)
+// {
+//     if (max_dep < depth[i])
+//     {
+//         max_dep = depth[i];
+//     }
+// }
+// cout << max_dep << endl;
+// return 0;
+// }
+
+//********************LCA in Tree*****************************
+// #include <bits/stdc++.h>
+// #define ll long long
+// #define int int64_t
+// using namespace std;
+// const int N = 1e5 + 10;
+// vector<int> g[N];
+// int par[N];
+// void DFS(int vertex, int parent = -1)
+// {
+//     par[vertex] = parent;
+//     for (auto x : g[vertex])
+//     {
+//         if (x == parent)
+//             continue;
+//         DFS(x, vertex);
+//     }
+// }
+// vector<int> path(int v)
+// {
+//     vector<int> ans;
+//     while (v != -1)
+//     {
+//         ans.push_back(v);
+//         v = par[v];
+//     }
+//     reverse(ans.begin(), ans.end());
+//     return ans;
+// }
+// int32_t main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int x, y;
+//         cin >> x >> y;
+//         g[x].push_back(y);
+//         g[y].push_back(x);
+//     }
+//     DFS(1);
+//     int p1, p2;
+//     cin >> p1 >> p2;
+//     vector<int> path_p1 = path(p1);
+//     vector<int> path_p2 = path(p2);
+//     int len = min(path_p1.size(), path_p2.size());
+//     int LCA = -1;
+//     for (int i = 0; i < len; i++)
+//     {
+//         if (path_p1[i] == path_p2[i])
+//             LCA = path_p1[i];
+
+//         else
+//             break;
+//     }
+//     cout << LCA << endl;
+//     return 0;
+// }
+
+//******************Edge Deletion Questions Basic*****************
+// #include <bits/stdc++.h>
+// #define ll long long
+// #define int int64_t
+// using namespace std;
+// const int N = 1e5 + 9;
+// vector<int> g[N];
+// int sum_subtree[N];
+
+// void DFS(int vertex, int parent = -1)
+// {
+//     sum_subtree[vertex] += vertex;
+//     for (auto x : g[vertex])
+//     {
+//         if (x == parent)
+//             continue;
+//         DFS(x, vertex);
+//         sum_subtree[vertex] += sum_subtree[x];
+//     }
+// }
+
+// int32_t main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(0);
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int x, y;
+//         cin >> x >> y;
+//         g[x].push_back(y);
+//         g[y].push_back(x);
+//     }
+//     DFS(1);
+//     int ans = -1;
+//     for (int i = 2; i <= n; i++)
+//     {
+//         int part1 = sum_subtree[i];
+//         int part2 = sum_subtree[1] - part1;
+//         ans = max(ans, part1 * part2);
+//     }
+//     cout << ans << endl;
+//     return 0;
+// }
